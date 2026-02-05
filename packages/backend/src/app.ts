c448192ce20be5f579adb6521ecc549fc08417a2
YaroslavUdych <slayes13@gmail.com>
2026-02-05 17:42:52 +0100

    backend: added middleware to disable caching for all responses

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -7,5 +7,10 @@ export const app = express()
 
 app.use(cors())
 app.use(express.json())
+// Disable caching for all responses
+app.use((_req, res, next) => {
+	res.setHeader('Cache-Control', 'no-store')
+	next()
+})
 
 app.use('/api/nodes', nodesRouter)

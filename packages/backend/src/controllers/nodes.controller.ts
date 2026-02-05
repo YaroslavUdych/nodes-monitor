6b341a09943d6a1c0ebfa3551c5640df53981abe
YaroslavUdych <slayes13@gmail.com>
2026-02-05 17:40:44 +0100

    backend: added try/catch to controller

--- packages/backend/src/controllers/nodes.controller.ts
+++ packages/backend/src/controllers/nodes.controller.ts
@@ -3,7 +3,11 @@ import type { Request, Response } from 'express'
 import { listNodes } from '../services/nodes.service.js'
 
 export function getNodes(_req: Request, res: Response) {
-	// Here recieves request and responds
-	const nodes = listNodes()
-	res.json(nodes)
+	try {
+		const nodes = listNodes()
+		res.status(200).json(nodes)
+	} catch (error) {
+		console.log('Failed to get nodes', error)
+		res.status(500).json({ message: 'Internal server error' })
+	}
 }
